fc4fb882a5ebaece8eb6df3257862e15211c840d
MrDoritos <ianmoore322@gmail.com>
2019-03-05 21:50:47 +0100

    w

--- console-agario/Program.cs
+++ console-agario/Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using gui;
+
 
 namespace console_agario
 {
     class Program
     {
+        //[DllImport("olcConsoleGameEngine.dll")]
+        //extern class olcConsoleGameEngine olcEngine;
+
         static void Main(string[] args)
         {
+            
             Game game = new Game(100, 100, 500);
             game.GameUpdateThread();
         }
